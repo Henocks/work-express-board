cf7ab587927b592c5639021a0c4f1a548cc0e5c2
Henocks <hr4574@naver.com>
2018-08-10 16:46:40 +0900

    DB connection done

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 const express = require('express');
 const dbconn = require('./libs/dbconn');
+const content = require('./libs/schema');
 
 const app = express();
 const mongoose = require('mongoose');
 dbconn.init(mongoose);
 
+const db = mongoose.connection;
+
 app.get('/', function (req, res) {
-  res.send("APIS RPC Broadcast server test page");
+  db.on('error', console.error.bind(console, 'connection error!'));
+  db.once('open', function(){
+    console.log('connection succesful');
+  })
+  res.send("test page");
 });
 
+app.get('/createPage', function (req, res) {
+  const page = new content(
+    {
+      writer: "Haryun",
+      password: "1234",
+      title: "TestPageTitle",
+      contents: "TestPageContent",
+      comments: [{
+          name: "Haryun2",
+          memo: "HAHAHAHAHA"
+      }]
+    }
+  );
+
+  page.save(function(err){
+    if(err) console.log(err);
+  });
+
+  
+
+  content.find(function(err, contents){
+    if(err) return console.error(err);
+    console.log(contents);
+    res.send(contents);
+  })
+
+  //res.send("TITLE : " + page.title);
+
+});
+
+app.get('/readPage', function (req, res) {
+
+  content.find(function(err, contents){
+    if(err) return console.error(err);
+    console.log(contents);
+    res.send(contents);
+  })
+
+  res.send(page);
+  //res.send("TITLE : " + page.title);
+
+});
 
 app.listen(3000, function(){
 	console.log("localhost:3000 Listening!");
